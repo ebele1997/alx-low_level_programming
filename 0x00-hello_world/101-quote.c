17e2c79e68db369432d39170dd5a977fa4b14b66
ebele1997 <ezegwu15@gmail.com>
2023-04-24 05:51:02 -0700

    101 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the stadard error
+ * main - A c program that prints a line to the standard error
  *
- * Return: 1 (Success)
+ * Return 1 (success)
  */
 int	main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout);
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 	return (1);
 }

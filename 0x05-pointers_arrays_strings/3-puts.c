18922235967335b223ff7e7e103a15ad384f1bd1
ebele1997 <ezegwu15@gmail.com>
2023-05-03 16:29:16 -0700

    Put files

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,16 @@
 #include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line, to stdout
- *
- * @str:string to print
+ * _puts - a function that prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
+{
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
